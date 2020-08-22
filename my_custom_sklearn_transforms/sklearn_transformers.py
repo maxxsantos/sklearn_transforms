750da2f34841759a3a8ff3b6b02017cf47c0eb03
Max Simões <max.santos@icomp.ufam.edu.br>
2020-08-22 01:38:19 -0400

    fix: change again

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -18,6 +18,7 @@ class DropColumns(BaseEstimator, TransformerMixin):
 class Balance(SMOTEENN):
     def __init__(self, sampling_strategy='auto', random_state=None, smote=None, enn=None, n_jobs=1, ratio=None):
         self = self
-    def fit_resample(self, X, y):
+    def fit(self, X, y):
         smote_enn = SMOTEENN(random_state=0)
+    def transform(self, X, y):
         return smote_enn.fit_resample(X, y)
